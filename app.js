876e5f82c561e5f69a7555ac0e00046369f94cb6
stefian <stefan.ianta@w3ai.net>
2020-05-15 09:35:47 -0300

    added products array var in admin for storing my prods/services

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false})); // to parse the req body sent through a form; not files, json, etc
 app.use(express.static(path.join(__dirname, 'public'))); // grant read access to the public folder
 
-app.use('/admin',adminRoutes); // /admin path filter
+app.use('/admin',adminData.routes); // /admin path filter
 app.use(shopRoutes);
 
 app.use((req, res, next) => { // catch all paths and methods
--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];  // pointer is constant, but we can add/remove elements
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
@@ -17,4 +19,5 @@ router.post('/add-product', (req, res, next) => {
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
