ca0866061404e33eee53f5294f0ea5b7c972bbc5
stefian <stefan.ianta@w3ai.net>
2020-05-16 13:51:32 -0300

    added page title in pug views

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => { // catch all paths and methods
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404');
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
