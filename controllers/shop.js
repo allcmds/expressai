88202e791383af7cb5ff835121fb7f98452f414a
stefian <stefan.ianta@w3ai.net>
2020-05-17 18:58:58 -0300

    added csrfToken in res.locals in app.js and _csrf Token in all POST form ejs

--- controllers/shop.js
+++ controllers/shop.js
@@ -37,9 +37,7 @@ exports.getIndex = (req, res, next) => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/',
-        isAuthenticated: req.session.isLoggedIn,
-        csrfToken: req.csrfToken()
+        path: '/'
       });
     })
     .catch(err => {
@@ -95,7 +93,7 @@ exports.postOrder = (req, res, next) => {
       });
       const order = new Order({
         user: {
-          name: req.user.name,
+          email: req.user.email,
           userId: req.user
         },
         products: products
