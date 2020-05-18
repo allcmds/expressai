3eb7a300bd7e33244597f5dd62670933bfd29bd9
stefian <stefan.ianta@w3ai.net>
2020-05-18 09:14:39 -0300

    added a flash error in auth for route /login

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,8 @@ const User = require('../models/user');
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    errorMessage: req.flash('error')
   });
 };
 
@@ -22,6 +23,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({email: email})
     .then(user => {
       if (!user) {
+        req.flash('error', 'Invalid email or password.');
         return res.redirect('/login')
       }
       bcrypt.compare(password, user.password)
