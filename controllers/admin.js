454ea1b16d7b8a6a3335c6737329a49cce1a932c
stefian <stefan.ianta@w3ai.net>
2020-05-19 15:46:31 -0300

    updated postDeleteProduct to work only for prods of the current user

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,7 +62,7 @@ exports.postEditProduct = (req, res, next) => {
 
   Product.findById(prodId)
     .then(product => {
-      if (product.userId !== req.user._id) {
+      if (product.userId.toString() !== req.user._id.toString()) {
         return res.redirect('/')
       }
       product.title = updatedTitle;
@@ -94,7 +94,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  // Product.findByIdAndRemove(prodId)
+  Product.deleteOne({ _id: prodId, userId: req.user._id })
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
