c17f36d0091fb8b4820a4b1dd5e175786e2db9bc
stefian <stefan.ianta@w3ai.net>
2020-05-21 09:26:47 -0300

    added validation for product editing

--- routes/admin.js
+++ routes/admin.js
@@ -17,7 +17,7 @@ router.get('/products', isAuth, adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', [
     body('title')
-        .isAlphanumeric()
+        .isString()
         .isLength({ min: 3 })
         .trim(),
     body('imageUrl').isURL(),
