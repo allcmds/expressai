84786baac928d244af82e1f0c302a3d7bdec55ad
stefian <stefan.ianta@w3ai.net>
2020-05-22 08:11:19 -0300

    added a fileFilter for Multer

--- app.js
+++ app.js
@@ -31,6 +31,18 @@ const fileStorage = multer.diskStorage({
   }
 }); // diskStorage = storage engine from multer
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/svg') {
+    cb(null, true); // no error, store
+  } else {
+    cb(null, false); // no error but do not store
+  }
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -39,7 +51,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
