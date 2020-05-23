6cba33b3877ec46afe0f78a7227515f112cd4f58
stefian <stefan.ianta@w3ai.net>
2020-05-23 07:52:02 -0300

    added deleteFile utility function

---
+++ util/file.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const deleteFile = (filePath) => {
+    fs.unlink(filePath, (err) => {      // delete file on server with unlink
+        if (err) {
+            throw (err);
+        }
+    });
+}
