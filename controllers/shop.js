604325235f274bb444225bf7fd2d6f514a80ed97
stefian <stefan.ianta@w3ai.net>
2020-05-23 17:06:38 -0300

    added dynamic page numbers for pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,7 +43,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  const page = req.query.page;
+  const page = +req.query.page;
   let totalItems;
 
   Product.find().countDocuments()
